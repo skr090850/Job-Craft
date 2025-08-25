f83443293e186642422a73a9fa0a625f80cf22f2
skr090850 <skr090850@gmail.com>
2025-08-25 18:04:22 +0530

    Ui complete of signup screen

--- app/src/main/java/com/example/jobcraft/registration/SignUpScreen.java
+++ app/src/main/java/com/example/jobcraft/registration/SignUpScreen.java
@@ -14,20 +14,18 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.jobcraft.R;
 import com.example.jobcraft.splash_and_onboarding.OnBoardingScreen;
 
-public class SignInScreen extends AppCompatActivity {
-    TextView signUpTxtBtn;
-
+public class SignUpScreen extends AppCompatActivity {
+    TextView signInTxtBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.registration_sign_in_screen);
-
-        signUpTxtBtn = findViewById(R.id.signUpTextBtn);
-        signUpTxtBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.registration_sign_up_screen);
+        signInTxtBtn = findViewById(R.id.signInTextBtn);
+        signInTxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignInScreen.this, SignUpScreen.class);
+                Intent intent = new Intent(SignUpScreen.this, SignInScreen.class);
                 startActivity(intent);
             }
         });
