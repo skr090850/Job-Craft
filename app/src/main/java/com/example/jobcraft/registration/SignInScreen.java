3c0beeaed53a77dcc7c3b3bf748734365a6c83d7
skr090850 <skr090850@gmail.com>
2025-08-25 18:54:42 +0530

    back button add and status baar theme error fix

--- app/src/main/java/com/example/jobcraft/registration/SignInScreen.java
+++ app/src/main/java/com/example/jobcraft/registration/SignInScreen.java
@@ -1,8 +1,10 @@
 package com.example.jobcraft.registration;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,12 +12,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.example.jobcraft.R;
 import com.example.jobcraft.splash_and_onboarding.OnBoardingScreen;
 
 public class SignInScreen extends AppCompatActivity {
     TextView signUpTxtBtn;
+    ImageButton signInBackBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +27,30 @@ public class SignInScreen extends AppCompatActivity {
         setContentView(R.layout.registration_sign_in_screen);
 
         signUpTxtBtn = findViewById(R.id.signUpTextBtn);
+        signInBackBtn = findViewById(R.id.signInBackBtn);
         signUpTxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignInScreen.this, SignUpScreen.class);
                 startActivity(intent);
+                finish();
             }
         });
+        signInBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController != null) {
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+                windowInsetsController.setAppearanceLightStatusBars(false);
+            } else {
+                windowInsetsController.setAppearanceLightStatusBars(true);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/jobcraft/registration/SignUpScreen.java
+++ app/src/main/java/com/example/jobcraft/registration/SignUpScreen.java
@@ -1,8 +1,10 @@
 package com.example.jobcraft.registration;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,23 +12,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.example.jobcraft.R;
 import com.example.jobcraft.splash_and_onboarding.OnBoardingScreen;
 
 public class SignUpScreen extends AppCompatActivity {
     TextView signInTxtBtn;
+    ImageButton signUpBackBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_sign_up_screen);
         signInTxtBtn = findViewById(R.id.signInTextBtn);
+        signUpBackBtn = findViewById(R.id.signUpBackBtn);
         signInTxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SignUpScreen.this, SignInScreen.class);
                 startActivity(intent);
+                finish();
             }
         });
+        signUpBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController != null) {
+            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+                windowInsetsController.setAppearanceLightStatusBars(false);
+            } else {
+                windowInsetsController.setAppearanceLightStatusBars(true);
+            }
+        }
     }
 }
