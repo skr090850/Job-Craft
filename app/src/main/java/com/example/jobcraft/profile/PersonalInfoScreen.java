920a5dec913c552d9145c94a1814a0a76775c97f
skr090850 <skr090850@gmail.com>
2025-08-31 17:15:20 +0530

    Ui completed of Personal profile screen

--- app/src/main/java/com/example/jobcraft/profile/PersonalInfoScreen.java
+++ app/src/main/java/com/example/jobcraft/profile/PersonalInfoScreen.java
@@ -1,7 +1,14 @@
 package com.example.jobcraft.profile;
 
+import android.app.DatePickerDialog;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +18,25 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
 
 import com.example.jobcraft.R;
+import com.example.jobcraft.databinding.ProfilePersonalInfoScreenBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class PersonalInfoScreen extends AppCompatActivity {
+    private ProfilePersonalInfoScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_personal_info_screen);
-
-
-
+        binding = ProfilePersonalInfoScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setDatePicker();
+        setupGenderDropdown();
 
 
         WindowInsetsControllerCompat windowInsetsController =
@@ -33,5 +49,44 @@ public class PersonalInfoScreen extends AppCompatActivity {
                 windowInsetsController.setAppearanceLightStatusBars(true);
             }
         }
+    }
+    private void setDatePicker(){
+        binding.profilePersonalDob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int year  = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        PersonalInfoScreen.this,
+                        new DatePickerDialog.OnDateSetListener(){
+                            public void onDateSet(DatePicker view, int selectYear, int selectMonth, int selectDay){
+                                Calendar selectDate=Calendar.getInstance();
+                                selectDate.set(selectYear,selectMonth,selectDay);
+                                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                                String formattedDate = dateFormat.format(selectDate.getTime());
+                                binding.profilePersonalDob.setText(formattedDate);
+                            }
+                        },
+                        year,
+                        month,
+                        day
+                );
+                datePickerDialog.show();
+            }
+        });
+    }
+    private void setupGenderDropdown() {
+        List<String> genders = Arrays.asList("Male", "Female", "Other");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                genders
+        );
+
+        binding.profilePersonalGender.setAdapter(adapter);
     }
 }
