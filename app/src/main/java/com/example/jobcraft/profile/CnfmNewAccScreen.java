d620dc70fdddab272c8da3d278355ba4803d2e5f
skr090850 <skr090850@gmail.com>
2025-09-01 21:21:20 +0530

    Completed UI of success screen

--- app/src/main/java/com/example/jobcraft/profile/CnfmNewAccScreen.java
+++ app/src/main/java/com/example/jobcraft/profile/CnfmNewAccScreen.java
@@ -1,6 +1,8 @@
 package com.example.jobcraft.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +11,24 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.jobcraft.R;
+import com.example.jobcraft.databinding.ProfileCnfmNewAccScreenBinding;
 
 public class CnfmNewAccScreen extends AppCompatActivity {
+    private ProfileCnfmNewAccScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.profile_cnfm_new_acc_screen);
+        binding = ProfileCnfmNewAccScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.cnfmNewAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CnfmNewAccScreen.this,SuccessScreen.class);
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
